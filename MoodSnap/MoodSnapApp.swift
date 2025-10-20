3936089ca36c90d23706cc1793e398020b47c0af
yelofoot <tyaus2@morgan.edu>
2025-10-19 22:00:30 -0400

    Initial Commit

---
+++ MoodSnap/MoodSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodSnapApp.swift
+//  MoodSnap
+//
+//  Created by Tyler Austin on 10/19/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
